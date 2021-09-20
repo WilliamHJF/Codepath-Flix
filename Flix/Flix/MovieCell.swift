5484386cd4263dd89a0f384a6ec75733076bf0a3
WilliamHJF <jihuo@ucsd.edu>
2021-09-20 15:46:41 -0700

    Upload code

---
+++ Flix/Flix/MovieCell.swift
@@ -0,0 +1,27 @@
+//
+//  MovieCell.swift
+//  Flix
+//
+//  Created by 霍一帆 on 9/20/21.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var synopsisLabel: UILabel!
+    @IBOutlet weak var posterView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Flix/Flix/MoviesViewController.swift
@@ -0,0 +1,91 @@
+//
+//  MoviesViewController.swift
+//  Flix
+//
+//  Created by 霍一帆 on 9/19/21.
+//
+
+import UIKit
+import AlamofireImage
+
+class MoviesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate{ //Step 2
+    
+    //Step 1
+    @IBOutlet weak var tableView: UITableView!
+    
+    var movies = [[String:Any]]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //Step 3
+        tableView.dataSource = self
+        tableView.delegate = self
+
+        // Do any additional setup after loading the view.
+        print("Hello")
+        
+        let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
+        let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        let task = session.dataTask(with: request) { (data, response, error) in
+             // This will run when the network request returns
+             if let error = error {
+                    print(error.localizedDescription)
+             } else if let data = data {
+                    let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
+                
+                    self.movies = dataDictionary["results"] as! [[String:Any]]
+                
+                    //Reload the data when the movies is downloaded completely
+                    self.tableView.reloadData()
+                    
+                    print(dataDictionary)
+
+                    // TODO: Get the array of movies
+                    // TODO: Store the movies in a property to use elsewhere
+                    // TODO: Reload your table view data
+
+             }
+        }
+        task.resume()
+    }
+    
+    //Step 2
+    //Asking number of rows
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return movies.count
+    }
+    
+    //For this particular row, give me the cell
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        //Create a new cell
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell") as! MovieCell
+        
+        let movie = movies[indexPath.row]
+        let title = movie["title"] as! String
+        let synopsis = movie["overview"] as! String
+        
+        cell.titleLabel!.text = title
+        cell.synopsisLabel!.text = synopsis
+        
+        let baseUrl = "https://image.tmdb.org/t/p/w500/"
+        let posterPath = movie["poster_path"] as! String
+        let posterUrl = URL(string: baseUrl + posterPath)
+        
+        cell.posterView.af.setImage(withURL: posterUrl!)
+        
+        return cell
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
