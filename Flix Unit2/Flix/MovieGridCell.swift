19a741814c88223a7da9c8d254a0df645ea48f96
WilliamHJF <jihuo@ucsd.edu>
2021-09-27 23:15:01 -0700

    Upload code for Unit2

---
+++ Flix Unit2/Flix/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by 霍一帆 on 9/27/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
